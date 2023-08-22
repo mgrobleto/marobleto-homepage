0fc346d5fc8dfd8640d2ee157d8b0ca6ad167fdb
Gabriela <magabriela0715@gmai.com>
2023-08-22 10:24:10 -0600

    removing photography section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,6 @@ export default function Home() {
         <Intro />
         <About />
         <Works />
-        <MyPhotography />
         <ContactMe />
         <SideBar />
       </ScrollObserver>
