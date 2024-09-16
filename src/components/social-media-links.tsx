0bd0412d6fb078b34e5f435ec48ae3f0030181af
Gabriela <magabriela0715@gmai.com>
2024-09-16 16:40:07 -0600

    updated cv link

--- src/components/social-media-links.tsx
+++ src/components/social-media-links.tsx
@@ -11,7 +11,7 @@ const SocialMedia : React.FC = () => {
         <div className="fixed xl:flex flex-col max-w-md h-auto px-10 bottom-10 bg-transparent right-2">
             <div className="flex flex-col justify-between items-end my-5">
                 <AnimatedSocialLink 
-                    href="https://drive.google.com/file/d/1N9pXX63lq5d_WgDiEIAxz9h4NtOG2YIc/view?usp=sharing" 
+                    href="https://drive.google.com/file/d/1za9uW_zO96zB5Ol7rV8bZoZxI5aPj8Jn/view?usp=sharing" 
                     icon={<ContactPageIcon fontSize="small" />}
                     classname="hover:text-[#92DEEF]"
                     color="#92DEEF"
